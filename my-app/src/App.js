9dd02f50fab02a3bb59dbda239d3fd96b33ae179
Sakshi08429 <chaudharysakshi5362@gmail.com>
2025-03-23 20:04:03 +0530

    using react

---
+++ my-app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Sakshi Chaudhary</h1>
+  );
+}
+
+export default App;
---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Component from './component';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <>
+    <Component/>
+    {/* <App /> */}
+    </>
+  </React.StrictMode>
+);
+
