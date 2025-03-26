0617049d5c3b93bf3def5611b83987338036831d
Sakshi08429 <chaudharysakshi5362@gmail.com>
2025-03-26 20:37:56 +0530

    include authentication services

---
+++ megaBlogAppWrite/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from "../confg/confg";
+import {Client,Account,ID} from "appwrite"
+
+
+export class AuthService{
+    client =new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(confg.appwriteUrl)
+        .setProject(appwriteProjectId);
+        this.account=new Account(this.client);
+    }
+
+    // appwrite services ka wrapper
+    async createAccount({email,password,name}){
+     try{
+        const userAccount=await this.account.create(ID.unique(),email,password,name)
+        if(userAccount){
+            //return userAccount;
+            //call another method;
+            return this.login({email,password});
+        }
+        else {
+            return userAccount;
+        }
+     }
+     catch(error){
+       throw error;
+     }
+    }
+
+    async login(){
+try{
+    return  await this.account.createEmailSession(email,password);
+
+}
+catch(error){
+    return error;
+}
+    }
+
+
+async getCurrentUser(){
+try{
+return await this.account.get();
+}
+catch(error){
+console.log("AppWrite service ::getCurrentUser:: error::  "+error);
+}
+return null;
+}
+
+async logout(){
+    try{
+        await this.account.deleteSessions();
+    }
+    catch(error){
+        console.log("error :: ", error);
+    }
+}
+}
+const authService =new AuthService();
+
+
+//export default AuthService;
+export default authService;
